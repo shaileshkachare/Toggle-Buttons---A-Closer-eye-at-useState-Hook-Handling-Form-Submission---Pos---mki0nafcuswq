8596b4da6d341122272a021afad4c1521f1013dd
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-22 09:05:34 +0530

    Assignment solve

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,37 @@ const App = () => {
   
 
   
-  return (
-    <div className="App">
-      <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
-      </button>
-      <br />
-      <br />
-      <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
-      </button>
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <button id="btn1" onClick={handleClick}>
+//       {/* assign value for button 1 */}
+//       </button>
+//       <br />
+//       <br />
+//       <button id="btn2" onClick={handleClick}>
+//         {/* assign value for button 2 */}
+//       </button>
+//     </div>
+//   );
+// }
+const [value,setValue]=useState(false)
+  
+const handleClick=()=>{
+  setValue(!value)
+  
+}
+return (
+  <div className="App">
+    <button id="btn1" onClick={handleClick}>
+    {value ? "ON" : "OFF"}
+    </button>
+    <br />
+    <br />
+    <button id="btn2" onClick={handleClick}>
+      {value ? "OFF" : "ON"}
+    </button>
+  </div>
+);
 }
 
 
